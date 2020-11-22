7c31ba3dfbbfb40f5ed94b01d6a04eee57f38476
Vova Pytsyuk <vovawed@gmail.com>
2020-11-23 00:23:35 +0200

    Added 2nd lab

---
+++ lab2/main.js
@@ -0,0 +1,14 @@
+function calculateHypotenuse() {
+
+    let kat1 = parseFloat(document.getElementById("kat1").value);
+    let kat2 = parseFloat(document.getElementById("kat2").value);
+
+    if (kat1 > 0 && kat2 > 0) {
+        let hypotenuse = Math.sqrt(
+            Math.pow(kat1, 2) + Math.pow(kat2, 2)
+        )
+        document.getElementById("result").innerText = "Катет: " + hypotenuse
+    } else {
+        alert("Ви помилились під час введення значення.\n Або ви ввели занадто велике значення катета \nПеревірте їх та повторіть спробу.");
+    }
+}
